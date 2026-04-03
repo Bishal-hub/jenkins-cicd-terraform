68d11c4a407be66b500eed2f66238df4a08f71a8
Bishal-hub <bishal1999sahoo@gmail.com>
2026-04-03 20:54:14 +0530

    changed the aws region of bucket and dynamodb

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-s3-bishal"
+    bucket         = "terraform-state-s3-bishal-bucket"
     key            = "ec2/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table-bishal"
